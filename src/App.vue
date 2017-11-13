a84695856031af525fc2674268cce7a597b9149d
wbeck <wbeck@nexon.com>
2017-11-13 11:42:54 -0800

    Added axios to use http request and exmaple.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+
+    <div class="get-list" v-if="hasResult">
+      <div v-for="post in posts" v-bind:key="post.id">
+        <h1>{{ post.id }}</h1>
+        <p>{{ post.title }}</p>
+      </div>
+    </div>
+
+    <button v-else v-on:click="searchTerm">Load Article</button>
+
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: function () {
+    return {
+      posts: []
+    }
+  },
+  computed: {
+    hasResult: function () {
+      return this.posts.length > 0
+    }
+  },
+  methods: {
+    searchTerm: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com'
+
+      this.$http.get(`${baseURI}/posts`)
+      .then((result) => {
+        console.log(result)
+        this.posts = result.data
+      })
+    }
+  }
 }
 </script>
 
@@ -19,5 +50,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.get-list {
+  text-align: left;
+}
+
+button {
+  display:block;
+  margin: 30px auto;
+}
+
+h1,
+p {
+  display: inline;
 }
 </style>
